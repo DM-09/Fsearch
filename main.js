b1b61ffab318619d11c977161e2b5a6095584023
DM <happydm09@naver.com>
2023-11-16 09:02:42 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+var url = ['https://search.naver.com/search.naver?query=', 'https://google.com/search?q=', 'https://terms.naver.com/search.naver?query=']
+
+function search(n) {
+  var q = document.querySelector("#search").value;
+  window.open(url[n] + q)
+}
+
+function getAIans() {
+  var el = document.querySelector("#res")
+  el.className = 'card c-body'
+  el.innerHTML = '<br> 답변 생성 중 <br> ​'
+  
+  var text = 'https://bardict-api.vercel.app/ai/' + document.getElementById("search").value
+  
+  const xhr = new XMLHttpRequest();
+ 
+  xhr.open('GET', text, true);
+  xhr.responseType = 'json';
+  xhr.send();
+
+  xhr.onload = function() {
+    const re = xhr.response;
+    document.getElementById("res").innerHTML = '<br>' + re.res + '<br>　' 
+  }
+}
